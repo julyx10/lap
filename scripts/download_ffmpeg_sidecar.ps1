37c88da5c6a2cda82be9a25fc69338fedfa4167b
Joseph Gershgorin <jgershgorin@biocentury.com>
2026-04-24 08:05:59 -0700

    Fix download url for ffmpeg on ARM64

--- scripts/download_ffmpeg_sidecar.ps1
+++ scripts/download_ffmpeg_sidecar.ps1
@@ -37,7 +37,7 @@ switch ($Arch) {
 }
 
 #$originalReleaseBase = "https://github.com/julyx10/lap-binaries/releases/download/ffmpeg-8.1"
-$ReleaseBase = "https://github.com/talynone/lap-binaries/releases/tag/ffmpeg-8.1"
+$ReleaseBase = "https://github.com/talynone/lap-binaries/releases/download/ffmpeg-8.1"
 
 $Downloads = @(
     "ffmpeg-$TripleSuffix.exe",
